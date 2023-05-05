b4f312239e78b0d5df97db6ec66a5e110dca6e3a
Gabriel Lins <gabrielinsmelo@gmail.com>
2023-05-05 17:51:27 -0300

    feat: :sparkles: Mensagens direcionadas para grupos

---
+++ grupos/kfkpy/__main__.py
@@ -0,0 +1,39 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import json
+
+
+SERVER_HOST = "localhost"
+SERVER_PORT = "9092"
+KAFKA_TOPIC = "quickstart-events"
+
+
+def value_serializer(m): return json.dumps(m).encode('utf-8')
+
+
+if __name__ == '__main__':
+    producer = KafkaProducer(
+        bootstrap_servers=[f'{SERVER_HOST}:{SERVER_PORT}'],
+        value_serializer=value_serializer)
+
+    with open("notification.json") as f:
+        data = json.load(f)
+
+    payload = {
+        "message": data['message'],
+        "metadata": data['metadata'],
+        "recipients": data['recipients'],
+    }
+
+    future = producer.send(KAFKA_TOPIC, payload)
+
+    # Block for 'synchronous' sends
+    try:
+        record_metadata = future.get(timeout=10)
+        print("Message successfully sent!")
+        print(f"topic: {record_metadata.topic}")
+        print(f"partition: {record_metadata.partition}")
+        print(f"offset: {record_metadata.offset}")
+
+    except KafkaError:
+        print("Message failed!")
